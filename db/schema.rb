684b81a476159eee4fa16205ff63521732d6c968
Lmilne <lmilne360@gmail.com>
2016-05-15 04:04:17 -0400

    ATTEMPT at adding user model from Github Wiki

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160328004905) do
+ActiveRecord::Schema.define(version: 20160515065909) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "link_id"
@@ -47,10 +47,12 @@ ActiveRecord::Schema.define(version: 20160328004905) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
   create_table "votes", force: :cascade do |t|
     t.integer  "votable_id"
